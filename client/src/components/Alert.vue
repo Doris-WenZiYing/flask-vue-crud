e9019330abcad88e688a5c61933d54e68427eb7e
Doris <doris070714@gmail.com>
2022-11-01 02:28:06 +0800

    v2

---
+++ client/src/components/Alert.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-alert variant="success" show>{{ message }}</b-alert>
+    <br>
+  </div>
+</template>
+
+<script>
+import { BAlert } from 'bootstrap-vue';
+
+export default {
+  props: ['message'],
+  components: {
+    BAlert,
+  },
+};
+</script>
